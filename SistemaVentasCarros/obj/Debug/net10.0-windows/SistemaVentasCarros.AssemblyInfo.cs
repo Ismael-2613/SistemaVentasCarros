96dc9a25ba909f8734a4e2671b6870d3aca7b02b
Ismael-2613 <ismaelbrenesgamer@gmail.com>
2026-07-09 08:41:42 -0600

    Fix gitignore definitivo

--- SistemaVentasCarros/obj/Debug/net10.0-windows/SistemaVentasCarros.AssemblyInfo.cs
+++ SistemaVentasCarros/obj/Debug/net10.0-windows/SistemaVentasCarros.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaVentasCarros")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9b43e84985a92b2500b930fd30f417dfb470bda")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaVentasCarros")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaVentasCarros")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
